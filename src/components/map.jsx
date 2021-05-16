686fda9c05229b0a0c99d2ea6d425ef49be4656e
noam k <noamk@noams-MacBook-Pro.local>
2021-05-16 12:34:14 +0300

    map via mapbox step 1

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Home.css";
+import ReactMapGL from "react-map-gl";
 
 const Map = () => {
-  return <div className="map"></div>;
+  const [viewPort, setViewPort] = useState({
+    latitude: -22.9032,
+    longitude: -43.1735,
+    zoom: 10,
+    width: "100vw",
+    height: "100vh",
+  });
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewPort}
+        mapboxApiAccessToken={
+          "pk.eyJ1Ijoia29ub2FtIiwiYSI6ImNrb3FydTcwczBkYjIydnA2ZmJtNGowZ24ifQ.wwF39UO0PuJRkBXrX0RyKw"
+        }
+        onViewportChange={setViewPort}
+      >
+        markrs here
+      </ReactMapGL>
+    </div>
+  );
 };
 
 export default Map;
