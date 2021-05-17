42e45771e59d7768c6bbc559469936acf60e4ebb
noam k <noamk@noams-MacBook-Pro.local>
2021-05-17 14:12:44 +0300

    adding server

---
+++ client-app/src/App.js
@@ -0,0 +1,22 @@
+import HeaderNavBar from "./components/headerNavbar";
+import HomePage from "./pages/HomePage";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <Container>
+      <header className="header">
+        <HeaderNavBar />
+      </header>
+      <main className="page-container">
+        <HomePage />
+      </main>
+
+      <footer className="footer"></footer>
+    </Container>
+  );
+};
+
+export default App;
---
+++ client-app/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+//components
+import Map from "../components/map";
+import ArtistsSideBar from "../components/artistsSideBar";
+import Filter from "../components/filter";
+//design
+import "../styles/Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+
+const HomePage = () => {
+  return (
+    <Container className="home-container">
+      <Row>
+        <Col md={6}>
+          <Map />
+          <Filter />
+        </Col>
+        <Col>
+          <ArtistsSideBar className="sideBar" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HomePage;
